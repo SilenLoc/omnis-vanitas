a279d96d103c5d99183a764bb38bb5635f7a3b0c
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2022-12-30 11:19:48 +0100

    chore: introduce serverready call

--- src/app.rs
+++ src/app.rs
@@ -1,87 +1,48 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-    open: bool,
+use std::sync::mpsc::{Receiver, Sender};
+use translation_server_client_silen::ready;
 
-    // this how you opt-out of serialization of a member
-    #[serde(skip)]
-    value: f32,
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+pub struct TemplateApp {
+    tx: Sender<String>,
+
+    rx: Receiver<String>,
+
+    open: bool,
+    translation_server_ready: String,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
+        let (tx, rx) = std::sync::mpsc::channel();
+
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
-            open: true,
+            tx,
+            rx,
+            open: false,
+            translation_server_ready: "nothing".to_owned(),
         }
     }
 }
 
 impl TemplateApp {
     /// Called once before the first frame.
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
-
+    pub fn new() -> Self {
         Default::default()
     }
 }
 
 impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
-
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let Self {
-            label,
-            value,
+            tx: _,
+            rx: _,
             open: _,
+            translation_server_ready,
         } = self;
-
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
-        #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        _frame.close();
-                    }
-                });
-            });
-        });
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
             ui.heading("Side Panel");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
             if ui.button("open a window").clicked() {
                 self.open = true;
             }
@@ -97,25 +58,38 @@ impl eframe::App for TemplateApp {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("Welcome");
-            ui.label("My github:");
-            ui.hyperlink("https://github.com/SilenLoc");
-
-            egui::warn_if_debug_build(ui);
+            ui.label(&*translation_server_ready);
         });
 
         if self.open {
             egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally choose either panels OR windows.");
                 if ui.button("Close").clicked() {
                     self.open = false;
                 }
+
+                if ui.button("is server ready?").clicked() {
+                    send_req(self.tx.clone(), ctx.clone());
+                }
             });
         }
+
+        if let Ok(ready_status) = self.rx.try_recv() {
+            self.translation_server_ready = ready_status;
+        }
     }
+}
+
+fn send_req(tx: Sender<String>, ctx: egui::Context) {
+    tokio::spawn(async move {
+        // Send a request with an increment value.
+        let response = ready().await;
+
+        let result = match response {
+            Ok(v) => v.text().await.map_err(|e| e.to_string()).unwrap(),
+            Err(e) => e,
+        };
+        let _ = tx.send(result);
+
+        ctx.request_repaint();
+    });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 #![warn(clippy::all, rust_2018_idioms)]
+use std::time::Duration;
+
 #[cfg(target_arch = "wasm32")]
 use omnis_vanitas::TemplateApp;
+use tokio::runtime::Runtime;
 
 // When compiling natively:
-#[cfg(not(target_arch = "wasm32"))]
+//[cfg(not(target_arch = "wasm32"))]
 fn main() {
+    let rt = Runtime::new().expect("Unable to create Runtime");
+
+    // Enter the runtime so that `tokio::spawn` is available immediately.
+    let _enter = rt.enter();
+
+    // Execute the runtime in its own thread.
+    // The future doesn't have to do anything. In this example, it just sleeps forever.
+    std::thread::spawn(move || {
+        rt.block_on(async {
+            loop {
+                tokio::time::sleep(Duration::from_secs(3600)).await;
+            }
+        })
+    });
+
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
 
@@ -12,16 +30,32 @@ fn main() {
     eframe::run_native(
         "omni vanitas",
         native_options,
-        Box::new(|cc| Box::new(omnis_vanitas::TemplateApp::new(cc))),
+        Box::new(|_| Box::new(omnis_vanitas::TemplateApp::new())),
     );
 }
 
 // when compiling to web using trunk.
 #[cfg(target_arch = "wasm32")]
 fn main() {
+
+    let rt = Runtime::new().expect("Unable to create Runtime");
+
+    // Enter the runtime so that `tokio::spawn` is available immediately.
+    let _enter = rt.enter();
+
+    // Execute the runtime in its own thread.
+    // The future doesn't have to do anything. In this example, it just sleeps forever.
+    std::thread::spawn(move || {
+        rt.block_on(async {
+            loop {
+                tokio::time::sleep(Duration::from_secs(3600)).await;
+            }
+        })
+    });
     // Make sure panics are logged using `console.error`.
 
     use eframe::{Theme, WebGlContextOption};
+    use futures::executor;
     console_error_panic_hook::set_once();
 
     // Redirect tracing to console.log and friends:
@@ -38,7 +72,7 @@ fn main() {
         eframe::start_web(
             "omni_vanitas_canvas", // hardcode it
             web_options,
-            Box::new(|cc| Box::new(TemplateApp::new(cc))),
+            Box::new(|_| Box::new(TemplateApp::new())),
         )
         .await
         .expect("failed to start eframe");
